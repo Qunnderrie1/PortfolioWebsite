0385df70e5921f2e5eaf13429441f697d842586a
Qunnderrie Snelling <qunnderrie@gmail.com>
2022-09-12 00:15:35 -0400

    added overflow-x to hidden

--- portfolioapp/src/Componets/Header.jsx
+++ portfolioapp/src/Componets/Header.jsx
@@ -5,10 +5,8 @@ import {motion} from "framer-motion"
 
 const Header = () => {
   return (
-    <motion.div 
-    initial={{ opacity: 0 , x: 400}} 
-    transition={{delay: 0.2}}
-    whileInView={{opacity: 1 , x: 0}}
+    <div 
+
      className='headerContainer container'>
         <h2 className='helloText'>&lt;Hello /&gt;</h2>
         <h3>I am</h3>
@@ -16,7 +14,7 @@ const Header = () => {
         <h3>Developer.</h3>
         <a className='btn resumeBtn  btn-primary' href='./files/QunnderrieSnelling-Developer.pdf' download>Resume</a>
 
-    </motion.div>
+    </div>
   )
 }
 
--- portfolioapp/src/Componets/NavBar.jsx
+++ portfolioapp/src/Componets/NavBar.jsx
@@ -3,6 +3,7 @@ import '../App.css'
 import {FiMenu }  from 'react-icons/fi'
 import { HiOutlineX}  from 'react-icons/hi'
 import { useState } from 'react'
+import { motion } from 'framer-motion'
 
 
 
@@ -25,7 +26,8 @@ const NavBar = () => {
     
 
     <div className='navContainer '>
-     <div 
+     <div  
+
      className={handle ? 'close container' : 'nav container'}>
       <div className="logo">
       <h4 className='logo'>&#x3D;&gt; Qunnderrie Snelling</h4>
@@ -36,7 +38,7 @@ const NavBar = () => {
         <li><a href='#about'>About</a></li>
         <li><a href='#contact'>Contact</a></li>
     </ul>
-    </div>
+    </div >
       <div onClick={NavBar} className='hamburgerMenu' >
        {
         handle ?  <FiMenu className='openMenu' color='white' size="40px" /> : <HiOutlineX className='closeMenu'  color='white' size="40px" />
